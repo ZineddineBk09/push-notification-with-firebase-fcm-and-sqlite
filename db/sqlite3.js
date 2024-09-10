0fb82aa0971883c393db0bc776798862a5592d97
ZineddineBk09 <eino1209@gmail.com>
2024-09-10 12:34:41 +0100

    db config

--- db/sqlite3.js
+++ db/sqlite3.js
@@ -1,5 +1,18 @@
 import { Sequelize } from "sequelize";
 
+// In case of using MySQL, uncomment the following lines
+/**import { MySqlDialect } from '@sequelize/mysql';
+
+const sequelize = new Sequelize({
+  dialect: MySqlDialect,
+  database: 'mydb',
+  user: 'myuser',
+  password: 'mypass',
+  host: 'localhost',
+  port: 3306,
+}); */
+
+// In case of using MySQL, comment the following lines
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "notifications.sqlite",
