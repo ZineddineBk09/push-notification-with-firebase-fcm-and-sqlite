86c8632e2d4db3a9bf73d3e0a9089cb055d7bfee
ZineddineBk09 <eino1209@gmail.com>
2024-09-10 12:34:33 +0100

    api routes

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  createPushNotification,
+  sendBatchNotification,
+  findAllPushNotifications,
+  findAllUnreadNotifications,
+  updatePushNotification,
+} from "../controllers/index.js";
+import { verifyToken } from "../middleware/authJwt.js";
+import {
+  sendBatchFCMNotificationMiddleware,
+  sendFCMNotificationMiddleware,
+} from "../middleware/fcm.js";
+
+const router = express.Router();
+
+router.use(verifyToken);
+
+// Push Notifications
+router.post(
+  "/notification",
+  sendFCMNotificationMiddleware,
+  createPushNotification
+);
+router.post(
+  "/notification/batch",
+  sendBatchFCMNotificationMiddleware,
+  sendBatchNotification
+);
+
+// router.get("/notification/:fcmToken", findAllPushNotifications);
+// router.get("/notification/unread/:fcmToken", findAllUnreadNotifications);
+// router.put("/notification", updatePushNotification);
+
+export default router;
